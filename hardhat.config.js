524e5c4178f34f9565ec040924274537711fbd5f
gdupont <gdupont.pro@gmail.com>
2023-05-30 12:32:37 +0200

    remove bad solution

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require('@nomicfoundation/hardhat-toolbox');
 require('@typechain/hardhat');
 require('@nomiclabs/hardhat-ethers');
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -8,6 +9,9 @@ module.exports = {
         compilers: [
             {
                 version: '0.5.6',
+            },
+            {
+                version: '0.8.17',
             },
             {
                 version: '0.8.15',
@@ -19,6 +23,14 @@ module.exports = {
                 },
             },
         ],
+    },
+    networks: {
+        polygon: {
+            url: 'https://rpc-mainnet.maticvigil.com',
+            accounts: [
+                '0xf3fcf7ba2295b525c8fcbe1618f267818645cb0b0ccf2ab7f48d14f4d972f72b',
+            ],
+        },
     },
     mocha: {
         timeout: 40000,
