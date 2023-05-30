21f9eb7867996be340ea381a19805b150c9b0141
gdupont <gdupont.pro@gmail.com>
2023-05-30 12:58:12 +0200

    feat: vote is optimized

--- test/Vote.js
+++ test/Vote.js
@@ -7,6 +7,7 @@ use(require('chai-as-promised'));
 
 const TARGET_GAS_PRICE = 136_508;
 const PROPOSAL_NAME = 'EXAMPLE';
+const PROPOSAL_NAME_BYTES32 = ethers.utils.formatBytes32String(PROPOSAL_NAME);
 const PROPOSAL_INDEX = 0;
 
 const logGasUsage = (currentGasUsage) => {
@@ -18,10 +19,6 @@ const logGasUsage = (currentGasUsage) => {
             `           You are \x1b[31m${diff * -1}\x1b[0m above the target`
         );
     }
-};
-
-const convertStringToBytes32 = (proposalName) => {
-    return ethers.utils.formatBytes32String(proposalName);
 };
 
 describe('Vote', async function () {
@@ -40,26 +37,21 @@ describe('Vote', async function () {
         it('The functions MUST remain non-payable', async function () {
             let error;
             try {
-                await instance.createProposal(
-                    convertStringToBytes32(PROPOSAL_NAME),
-                    {
-                        value: ethers.utils.parseEther('1.00'),
-                    }
-                );
+                await instance.createProposal(PROPOSAL_NAME_BYTES32, {
+                    value: ethers.utils.parseEther('1.00'),
+                });
             } catch (e) {
                 error = e;
             }
-
             expect(error.reason).to.equal(
                 'non-payable method cannot override value'
             );
             expect(error.code).to.equal('UNSUPPORTED_OPERATION');
-            expect(
-                instance.createProposal(convertStringToBytes32(PROPOSAL_NAME))
-            ).to.not.be.rejected;
+            expect(instance.createProposal(PROPOSAL_NAME_BYTES32)).to.not.be
+                .rejected;
 
             try {
-                await instance.vote(PROPOSAL_INDEX, {
+                await instance.vote(PROPOSAL_NAME_BYTES32, {
                     value: ethers.utils.parseEther('1.00'),
                 });
             } catch (e) {
@@ -76,13 +68,11 @@ describe('Vote', async function () {
 
     describe('Gas target', function () {
         it('The functions MUST meet the expected gas efficiency', async function () {
-            const tx1 = await instance.createProposal(
-                convertStringToBytes32(PROPOSAL_NAME)
-            );
+            const tx1 = await instance.createProposal(PROPOSAL_NAME_BYTES32);
             const receipt1 = await tx1.wait();
             const gasEstimateTx1 = receipt1.gasUsed;
 
-            const tx2 = await instance.vote(PROPOSAL_INDEX);
+            const tx2 = await instance.vote(PROPOSAL_NAME_BYTES32);
             const receipt2 = await tx2.wait();
             const gasEstimateTx2 = receipt2.gasUsed;
 
@@ -96,19 +86,17 @@ describe('Vote', async function () {
 
     describe('Business logic', function () {
         it('The functions MUST perform as expected', async function () {
-            await instance.createProposal(
-                convertStringToBytes32(PROPOSAL_NAME)
-            );
-            let voteCount = await instance.getVoteCount(PROPOSAL_INDEX);
+            await instance.createProposal(PROPOSAL_NAME_BYTES32);
+            let voteCount = await instance.getVoteCount(PROPOSAL_NAME_BYTES32);
             expect(voteCount).equal(0);
 
-            await instance.vote(PROPOSAL_INDEX);
-            voteCount = await instance.getVoteCount(PROPOSAL_INDEX);
+            await instance.vote(PROPOSAL_NAME_BYTES32);
+            voteCount = await instance.getVoteCount(PROPOSAL_NAME_BYTES32);
             expect(voteCount).to.equal(1);
 
-            await expect(instance.vote(PROPOSAL_INDEX)).to.be.revertedWith(
-                'already voted'
-            );
+            await expect(
+                instance.vote(PROPOSAL_NAME_BYTES32)
+            ).to.be.revertedWith('already voted');
         });
     });
 });
